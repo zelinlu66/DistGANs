d607f9b41b3039da1bec1b0f52fab6144bfcc7ba
Massimiliano Lupo Pasini <lupopasinim@ornl.gov>
2020-04-25 11:37:58 -0400

    cifar 10 transformation rolled back to version uploaded by Vittorio

--- Dataloader.py
+++ Dataloader.py
@@ -26,7 +26,7 @@ def mnist_data():
 
 def cifar10_data():
     compose = transform = transforms.Compose([transforms.ToTensor(), 
-                                              transforms.Normalize((0.5,), (0.5,))
+                                              transforms.Normalize((.5,.5,.5), (.5,.5,.5))
                                               ])
     out_dir = './dataset'
     return datasets.CIFAR10(root=out_dir, train=True, transform=compose, download=True)
@@ -47,7 +47,7 @@ def cifar_data_dcgans():
         [
             transforms.Resize(64),
             transforms.ToTensor(),
-            transforms.Normalize((0.5,), (0.5,))
+            transforms.Normalize((.5,.5,.5), (.5,.5,.5))
         ])
     out_dir = '{}/dataset'.format(os.getcwd())
     return datasets.CIFAR10(root=out_dir, train=True, transform=compose, download=True)
