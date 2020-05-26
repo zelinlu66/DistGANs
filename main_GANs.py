eb7db0b3566a32a4919f5f6ad761d5b1b3062986
Vitaliy Starchenko <vitaliy.starchenko@gmail.com>
2020-05-25 20:54:03 -0400

    added additional argument options to main

--- main_GANs.py
+++ main_GANs.py
@@ -10,21 +10,29 @@ Usage:
   main_GANs.py --version
   main_GANs.py [--epochs=100] [--optimizer='CGD'] [--learning_rate=0.0005]
   main_GANs.py [-e 100] [-o 'CGD'] [-l 0.0005]
+  main_GANs.py [--model='MLP']
+  main_GANs.py [-m 'CNN'] [--display] [--save]
+
   
 Options:
   -h, --help                  Show this screen.
   --version                   Show version.
+  --display                   Use matplotlib to plot results.
+  --save                      Save a model.
   -e, --epochs=<n>            Number of epochs [default: 100].
   -o, --optimizer=<str>       Optimizer name [default: Jacobi].
   -l, --learning_rate=<f>     Learning rate [default: 0.01].
+  -m, --model=<str>           Implementation of GANs model. Multi-layer 
+                              perceptrons NN (MLP), convolutional NN (CNN) 
+                              [default: MLP].
 """
-
+import sys
 from docopt import docopt
 from MLP_GANs_object import *
 from DCGANs_object import *
 '''
 ! READ ME !
-
+Multi-layer perceptrons neural networks (MLP), convolutional neural networks (CNN) 
 X = Generator
 Y = Discriminaror
 
@@ -55,29 +63,37 @@ if __name__ == '__main__':
     epochs = int(arguments['--epochs'])
     optimizer_name = arguments['--optimizer']
     learning_rate = float(arguments['--learning_rate'])
+    model_switch = arguments['--model']
 
-    model = MLP_GANs_model(mnist_data(rand_rotation = False, max_degree = 90))
+    if model_switch=='MLP':
+        print("Using MLP implementation of GANs: MLP_GANs_model")
+        model = MLP_GANs_model(mnist_data(rand_rotation = False, max_degree = 90))
+    elif model_switch=='CNN':
+        print("Using CNN implementation of GANs: DCGANs_model")
+        model = DCGANs_model(mnist_data_dcgans(rand_rotation = False, max_degree = 90))
+    else:
+        sys.exit('\n   *** Error. Specified model name: {} is not valid. Please choose MLP or CNN'.format(model_switch))
+        
     model.train(num_epochs = epochs, lr_x = torch.tensor([learning_rate]), lr_y = torch.tensor([learning_rate]),
-                optimizer_name = optimizer_name, verbose = True, label_smoothing = False, single_number = 4) # save_path = ''
+             optimizer_name = optimizer_name, verbose = True, label_smoothing = False, single_number = 4) # save_path = ''
 
-    #model = DCGANs_model(mnist_data_dcgans(rand_rotation = False, max_degree = 90))
-    #model.train(num_epochs = epochs, lr_x = torch.tensor([learning_rate]), lr_y = torch.tensor([learning_rate]),
-#                optimizer_name = optimizer_name, verbose = True, label_smoothing = False, single_number = 9) #save_path = ''
+    if arguments['--save']:
+        print("Saving the model...")
+        model.save_models()
 
-    #model.save_models()
-
-    plt.figure()
-    plt.plot([x for x in range(0, len(model.D_error_real_history))],
-             model.D_error_real_history)
-    plt.plot([x for x in range(0, len(model.D_error_fake_history))],
-             model.D_error_fake_history)
-    plt.plot([x for x in range(0, len(model.G_error_history))],
-             model.G_error_history)
-    plt.xlabel('Iterations')
-    plt.ylabel('Loss function value')
-    plt.legend([
-        'Discriminator: Loss on Real Data', 'Discriminator: Loss on Fake Data',
-        'Generator: Loss'
-    ])
-    plt.savefig('cost_report.png')
+    if arguments['--display']:
+        plt.figure()
+        plt.plot([x for x in range(0, len(model.D_error_real_history))],
+                 model.D_error_real_history)
+        plt.plot([x for x in range(0, len(model.D_error_fake_history))],
+                 model.D_error_fake_history)
+        plt.plot([x for x in range(0, len(model.G_error_history))],
+                 model.G_error_history)
+        plt.xlabel('Iterations')
+        plt.ylabel('Loss function value')
+        plt.legend([
+            'Discriminator: Loss on Real Data', 'Discriminator: Loss on Fake Data',
+            'Generator: Loss'
+        ])
+        plt.savefig('cost_report.png')
 
